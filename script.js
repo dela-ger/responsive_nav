b76bc3251a44c820f006e6cbb51da373a15c07e0
dela-ger <96391704+dela-ger@users.noreply.github.com>
2022-03-09 17:57:17 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+const toggleBtn = document.getElementsByClassName('toggle-btn')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleBtn.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
